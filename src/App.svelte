f7399bb2ae01822467226babc7f1d0a2bb427004
John <jakebritti@gmail.com>
2021-04-13 00:32:06 -0400

    initial

---
+++ src/App.svelte
@@ -0,0 +1,202 @@
+<script>
+	import ChordPlayer from "./ChordPlayer.svelte";
+	import scale from "./scale.svg";
+	import check from "./check.svg";
+</script>
+
+<main>
+	<div class="sidebar">
+		<div class="section">
+			<h1>Writing Chord Progressions</h1>
+			<div class="subsection">
+				<h2>Lesson 1</h2>
+				<h3>Introduction</h3>
+				<div class="completed" class:visible={true}>
+					<img class="check" src={check} alt="" />
+				</div>
+			</div>
+			<div class="subsection">
+				<h2>Lesson 2</h2>
+				<h3>Expanding the repertoire</h3>
+				<div class="completed" class:visible={false}>
+					<img class="check" src={check} alt="" />
+				</div>
+			</div>
+		</div>
+		<div class="section">
+			<h1>Progressions to Movements</h1>
+			<div class="subsection">
+				<h2>Lesson 1</h2>
+				<h3>Example</h3>
+				<div class="completed" class:visible={false}>
+					<img class="check" src={check} alt="" />
+				</div>
+			</div>
+			<div class="subsection">
+				<h2>Lesson 2</h2>
+				<h3>Example</h3>
+				<div class="completed" class:visible={false}>
+					<img class="check" src={check} alt="" />
+				</div>
+			</div>
+		</div>
+	</div>
+	<header>
+		<div class="content">
+			<h1>Writing Chord Progressions</h1>
+			<h2>Introduction</h2>
+		</div>
+	</header>
+	<article>
+		<p>
+			At their most basic level, chord progressions are a series of two or more chords played
+			together in succession. Understanding how to construct chord progressions is essential because
+			they are the foundation of the tone and narrative of a song. Chord progression can be used to
+			convey moods that are happy, sad, emotional, optimistic, adventurous, and more. Let’s discuss
+			a foundational framework for building chord progressions, then have you build some on your
+			own.
+		</p>
+		<p>
+			First, let’s introduce our virtual writing interface. Below you’ll see seven circles with
+			roman numerals (we’ll explain these letters are in a bit), each representing a different
+			chord. Underneath them are four squares, which are slots where you can add the chords from
+			above. Each slot holds one chord, allowing you to put four chords in succession. Try dragging
+			a chord to each block, then hit the play button to hear the chord progression you’ve made.
+		</p>
+		<ChordPlayer />
+		<p>
+			Each roman numeral above represents a diatonic triad in a major key. To refresh your memory on
+			these concepts:
+		</p>
+
+		<ul>
+			<li>
+				<a href="https://en.wikipedia.org/wiki/Major_scale" target="_blank">Major Key</a>: A
+				specific grouping of seven notes.
+			</li>
+			<li>
+				<a href="https://www.musictheory.net/lessons/43" target="_blank">Diatonic Triads</a>: Seven
+				chords built on each of the notes in the chosen major or minor key.
+			</li>
+		</ul>
+		<p>
+			Each roman numeral represents a different diatonic triad, with the number corresponding to the
+			<a href="https://en.wikipedia.org/wiki/Degree_(music)" target="_blank">scale degree</a> that chord
+			is built on. Major chords are capitalized, minor chords are lowercase, and diminished chords are
+			lowercase with a superscript ‘o’. Below are the seven diatonic triads in a major key.
+		</p>
+		<img src={scale} style="width: 100%" alt="" />
+		<p>
+			However, as you may have experienced earlier when building chord progressions above, not all
+			chord progressions are alike. Some chord progressions sound happy, others sound sad, some
+			sounds smooth, while others sound more dissonant.
+		</p>
+
+		<p>
+			To help understand why certain chord progressions ‘work’, let’s discuss an important concept
+			called <a href="https://www.artofcomposing.com/08-diatonic-harmony" target="_blank"
+				><i>functional harmony</i></a
+			>. The central idea behind functional harmony is each diatonic chord serves a specific
+			function. Some chords sound ‘stable’ and ‘at home’, some sound ‘unstable’ and ‘tense’, and
+			others sound like a ‘departure’.
+		</p>
+	</article>
+</main>
+
+<style>
+	main {
+		display: grid;
+		grid-template-columns: 300px 1fr minmax(auto, 900px) 1fr 300px;
+		grid-template-areas:
+			"sidebar . header . ."
+			"sidebar . article . .";
+		grid-template-rows: auto 1fr;
+		gap: 20px;
+		height: 100%;
+	}
+	@media screen and (max-width: 1600px) {
+		main {
+			grid-template-columns: 300px auto minmax(auto, 900px) auto auto;
+		}
+	}
+	.sidebar {
+		grid-area: sidebar;
+		background: #fafafa;
+		border-right: 1px solid #e4e4e4;
+	}
+	.sidebar .section {
+		padding-left: 15px;
+		margin-top: 20px;
+		color: #999999;
+	}
+	.section h1 {
+		font-size: 20px;
+		border-bottom: 1px solid #e4e4e4;
+		font-weight: 300;
+		margin: 0 0 15px 10px;
+		padding-bottom: 5px;
+	}
+	.subsection {
+		display: grid;
+		grid-template-areas: "lesson completed" "title completed";
+		grid-template-columns: 1fr auto;
+		grid-template-rows: auto auto;
+		padding: 5px 7px;
+		margin-left: 3px;
+		margin-bottom: 5px;
+		cursor: pointer;
+	}
+	.subsection:hover {
+		background: #0001;
+	}
+	.section h2 {
+		font-size: 16px;
+		font-weight: 500;
+		margin: 0;
+		grid-area: lesson;
+		align-self: end;
+	}
+	.section h3 {
+		font-size: 16px;
+		font-weight: 300;
+		margin: 0;
+		grid-area: title;
+	}
+	.completed {
+		grid-area: completed;
+		display: none;
+		align-self: center;
+	}
+	.completed.visible {
+		display: inline;
+	}
+	.completed img {
+		width: 15px;
+		margin: 6px;
+		padding: 0;
+	}
+	header {
+		grid-area: header;
+		padding: 80px 0 20px;
+		/* display: grid; */
+		/* grid-template-columns: 1fr minmax(auto, 900px) 1fr 2fr; */
+		/* background: #704fac;
+		color: #fff; */
+	}
+	header .content {
+		grid-column-start: 2;
+	}
+	header h1 {
+		font-size: 1.5em;
+		font-weight: 300;
+		margin: 0;
+	}
+	header h2 {
+		font-size: 4.5em;
+		margin: 0;
+	}
+	article {
+		grid-area: article;
+		text-align: justify;
+	}
+</style>
---
+++ src/ChordPlayer.svelte
@@ -0,0 +1,210 @@
+<script>
+	import { onMount } from "svelte";
+	import { spring } from "svelte/motion";
+	import { foo } from "./test.js";
+	import * as Tone from "tone";
+
+	const synth = new Tone.PolySynth().toDestination();
+	synth.volume.value = -6;
+	const offsets = spring([0, 0, 0, 0, 0], { stiffness: 0.2, damping: 0.4 });
+	let chords = ["c", "d", "e", "f", "g", "a", "b"];
+	let colors = {
+		c: "#F6BC46",
+		d: "#F6BC46",
+		e: "#68C9CF",
+		f: "#EF8F9B",
+		g: "#F6BC46",
+		a: "#68C9CF",
+		b: "#68C9CF",
+	};
+	let loop = true;
+	var seq;
+	let active = null;
+
+	$: phrase = [
+		{ chord: "c", notes: [], degree: "4", length: 4, offset: 0 },
+		{ chord: "c", notes: [], degree: "4", length: 4, offset: 0 },
+		{ chord: "c", notes: [], degree: "4", length: 4, offset: 0 },
+		{ chord: "c", notes: [], degree: "4", length: 4, offset: 0 },
+		// { chord: "c", degree: "4", length: 4, offset: 0 },
+		// { chord: "c", degree: "4", length: 4, offset: 0 },
+	];
+
+	function mod(n, m) {
+		return ((n % m) + m) % m;
+	}
+	function handlePanStart(event, i) {
+		// 		phrase[i].offset.stiffness = 1;
+		// 		phrase[i].offset.damping = 1;
+	}
+
+	onMount(() => {
+		seq = new Tone.Sequence(
+			function (time, note) {
+				synth.triggerAttackRelease(["C4", "E4", "G4"], "16n", time);
+				active = note.index;
+			},
+			phrase.map((chord, i) => ({ note: chord.chord + chord.degree, index: i })),
+			"4n"
+		);
+		seq.start(0);
+		Tone.start();
+	});
+
+	function handlePanMove(event, i) {
+		phrase[i].offset = Math.min(Math.max(phrase[i].offset + event.detail.dy, -30 * 3), 30 * 3);
+		let degree = "4";
+		if (-Math.round(phrase[i].offset / 30) < 0) {
+			degree = "3";
+		}
+		phrase[i].degree = degree;
+		phrase[i].chord = chords[mod(-Math.round(phrase[i].offset / 30), chords.length)];
+	}
+
+	function handlePanEnd(event, i) {
+		let degree = "4";
+		if (-Math.round(phrase[i].offset / 30) < 0) {
+			degree = "3";
+		}
+		if (!playing)
+			synth.triggerAttackRelease(
+				chords[mod(-Math.round(phrase[i].offset / 30), chords.length)] + degree,
+				"4n"
+			);
+		seq.events[i] = {
+			note: chords[mod(-Math.round(phrase[i].offset / 30), chords.length)] + degree,
+			index: i,
+		};
+		// seq.set(chords[mod(-Math.round(phrase[i].offset / 30), chords.length)] + degree);
+		// seq.dispose();
+		// seq = new Tone.Sequence(
+		// 	function (time, note) {
+		// 		synth.triggerAttackRelease(note, 0.1, time);
+		// 		//straight quater notes
+		// 	},
+		// 	phrase.map((chord) => chord.chord + chord.degree),
+		// 	"4n"
+		// ).start(0);
+	}
+
+	let playing = false;
+
+	function play() {
+		// const loop = new Tone.Loop((time) => {
+		// 	synthA.triggerAttackRelease("C4", "8n", time);
+		// 	synthA.triggerAttackRelease("D4", "8n", time + 0.2);
+		// 	synthA.triggerAttackRelease("E4", "8n", time + 0.4);
+		// 	synthA.triggerAttackRelease("F4", "8n", time + 0.6);
+		// }, "4n").start(0);
+		Tone.Transport.toggle();
+		playing = !playing;
+	}
+</script>
+
+<svelte:window
+	on:keydown={(e) => {
+		if (e.keyCode === 32) play();
+	}}
+/>
+
+<main>
+	<div class="editor">
+		<div class="buttons">
+			<button class="play" on:click={play}>{playing ? "■" : "▶"}</button>
+			<button class="loop" class:active={loop} on:click={() => (loop = !loop)}>
+				<svg viewBox="0 0 26.6 26.4" width="20px"
+					><path class="st0" d="M25.5,21.3h-17c-4.2,0-7.5-3.4-7.5-7.5v-2.5" />
+					<path class="st0" d="M1.1,5.1h17c4.2,0,7.5,3.4,7.5,7.5v2.5" />
+					<polyline class="st0" points="21.4,17.1 25.5,21.3 21.4,25.4 " />
+					<polyline class="st0" points="5.2,9.2 1.1,5.1 5.2,1 " /></svg
+				>
+				<!-- <img src="./loop.svg" alt="" width="20px" /> -->
+			</button>
+		</div>
+		<div class="phrase">
+			{#each phrase as chord, i}
+				<div
+					class="chord"
+					class:active={playing && i === active}
+					use:foo
+					on:panstart={(e) => handlePanStart(e, i)}
+					on:panmove={(e) => handlePanMove(e, i)}
+					on:panend={(e) => handlePanEnd(e, i)}
+					style="background: {colors[chord.chord]}; top: {Math.round(phrase[i].offset / 30) *
+						30}px;"
+				>
+					<h2>{chord.chord.toUpperCase()}</h2>
+				</div>
+			{/each}
+		</div>
+	</div>
+</main>
+
+<style>
+	main {
+		padding: 120px 20px;
+		margin: 0 auto;
+		max-width: 1000px;
+		background: #f3f3f3;
+		user-select: none;
+	}
+	.editor {
+		display: grid;
+		grid-template-columns: auto 1fr;
+		gap: 15px;
+		/* height: 75px; */
+	}
+	.buttons {
+		display: grid;
+		align-items: center;
+		justify-items: center;
+		padding: 10px 0 10px 0;
+	}
+	.buttons button {
+		margin: 0;
+		background: none;
+		border: none;
+		padding: 0;
+		cursor: pointer;
+		transition: transform 0.15s ease;
+	}
+	.buttons button:hover {
+		transform: scale(1.2);
+	}
+	.phrase {
+		display: grid;
+		grid-template-columns: repeat(auto-fit, minmax(20px, 1fr));
+		gap: 10px;
+	}
+	.chord {
+		text-align: center;
+		background: rgb(224, 67, 94);
+		border-radius: 4px;
+		color: #333d;
+		user-select: none;
+		cursor: grab;
+		transition: background 0.15s ease, border 0.1s ease;
+		display: grid;
+		align-content: center;
+		box-sizing: border-box;
+		position: relative;
+		border: 5px solid #3330;
+	}
+	.active {
+		/* height: 100px; */
+		/* transform: scale(1.05); */
+		border: 5px solid #333d;
+	}
+	.chord:active {
+		cursor: grabbing;
+	}
+	.loop.active * {
+		stroke: #6e6ecc;
+	}
+	svg * {
+		fill: none;
+		stroke-width: 2.5px;
+		stroke: #333;
+		transition: stroke 0.15s ease;
+	}
+</style>
